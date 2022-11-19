b96023f64c2166eeba45e9b29194fd3546a0b945
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2022-11-19 20:11:13 -0300

    tela carregamento

--- shopping_cart/lib/main.dart
+++ shopping_cart/lib/main.dart
@@ -1,6 +1,20 @@
-
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
-  runApp();
+  runApp(const GetMaterialApp(home: MeuApp()));
 }
 
+class MeuApp extends StatelessWidget {
+  const MeuApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Text('agua'),
+    );
+  }
+}
