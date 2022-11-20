699481b05042cb22f0c9720ef61d88a6d5add48d
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2022-11-20 11:30:29 -0300

    stful

--- shopping_cart/lib/tela_carrinho.dart
+++ shopping_cart/lib/tela_carrinho.dart
@@ -3,3 +3,17 @@ import 'package:flutter/cupertino.dart';
 main() {
   runApp(app);  
 }
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
