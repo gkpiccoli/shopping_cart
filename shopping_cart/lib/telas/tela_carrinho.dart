2b0dd61f834a17934e822ca96abe2bb7261598e1
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2022-11-21 10:35:37 -0300

    classe produtos

--- shopping_cart/lib/telas/tela_carrinho.dart
+++ shopping_cart/lib/telas/tela_carrinho.dart
@@ -1,19 +1,8 @@
-import 'package:flutter/cupertino.dart';
+class Produto {
+  final String nome;
+  final String preco;
+  final String urlImagem;
 
-main() {
-  runApp(app);  
-}
-
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
-
-  @override
-  State<MyWidget> createState() => _MyWidgetState();
-}
-
-class _MyWidgetState extends State<MyWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+  const Produto(
+      {required this.nome, required this.preco, required this.urlImagem});
 }
