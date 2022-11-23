b6264b000438c46f0ed22c2d26fbd6ec68ba176a
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2022-11-23 13:11:55 -0300

    upd

--- shopping_cart/lib/telas/tela_catalogo.dart
+++ shopping_cart/lib/telas/tela_catalogo.dart
@@ -1,23 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-
-import '../widgets/catalogo_produtos.dart';
 
 class TelaCatalogo extends StatelessWidget {
   const TelaCatalogo({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Catálogo')),
-      body: SafeArea(
-          child: Column(
-        children: [
-          const CatalogoProdutos(),
-          ElevatedButton(onPressed: () => Get.to(() => TelaCatalogo()), child: const Text('Voltar ao Carrinho'))
-        ],
-      )),
-    );
+    return Container();
   }
 }
 
+class CatalogoProdutos extends StatelessWidget {
+  const CatalogoProdutos({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Catálogo')),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const CatalogoProdutos(),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Voltar ao Carrinho'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- shopping_cart/lib/widgets/catalogo_produtos.dart
+++ shopping_cart/lib/widgets/catalogo_produtos.dart
@@ -1,19 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_cart/modelos/modelo_produto.dart';
 
-class CatalogoProdutos extends StatelessWidget {
-  const CatalogoProdutos({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Flexible(child: ListView.builder(
-      itemCount: Produto.produtos.item,
-      itemBuilder: itemBuilder));
-  }
-}
 
-class CartaoCatalogoDeProdutos extends StatelessWidget {
-  const CartaoCatalogoDeProdutos({super.key});
+
+
+class CartaoProdutos extends StatelessWidget {
+  const CartaoProdutos({super.key});
 
   @override
   Widget build(BuildContext context) {
