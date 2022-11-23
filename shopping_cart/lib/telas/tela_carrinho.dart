38151828ae292d084630cd7a6bd3df0cf86e6aea
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2022-11-22 21:24:13 -0300

    update tela_carrinho.dart

--- shopping_cart/lib/telas/tela_carrinho.dart
+++ shopping_cart/lib/telas/tela_carrinho.dart
@@ -20,7 +20,7 @@ const List<Produto> produtos = [
           'https://images.unsplash.com/photo-1513558161293-cdaf765ed2fd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80'),
   Produto(
       nome: 'Refrigerante',
-      preco: 4.99,
+      preco: 3.99,
       urlImagem:
           'https://images.unsplash.com/photo-1566846128021-b940b0eec910?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80')
 ];
