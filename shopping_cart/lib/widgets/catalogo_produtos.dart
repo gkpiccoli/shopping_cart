654ef367dbb938e9ec939a9c4cbd1f62af22bfb6
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2022-11-23 10:59:31 -0300

    catalogo

---
+++ shopping_cart/lib/widgets/catalogo_produtos.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CatalogoDeProdutos extends StatelessWidget {
+  const CatalogoDeProdutos({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
